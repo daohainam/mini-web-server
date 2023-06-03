654c2e0d22be8b9da2f8a27382fe3c40cdf9cdb7
daohainam <daohainam@gmail.com>
2023-06-03 22:46:41 +0200

    Support cookie properties

--- MiniWebServer.Abstractions/Http/HttpCookie.cs
+++ MiniWebServer.Abstractions/Http/HttpCookie.cs
@@ -8,12 +8,14 @@ namespace MiniWebServer.Abstractions.Http
 {
     public sealed class HttpCookie
     {
-        public HttpCookie(string name, string value, 
-            DateTime? expires = default,
-            bool? secure = default,
+        public HttpCookie(string name, string value,
+            string? domain = default,
+            DateTimeOffset? expires = default,
             bool? httpOnly = default,
             long? maxAge = default,
-            string? path = default
+            string? path = default,
+            SameSitePolicies? sameSite = default,
+            bool? secure = default
             )
         {
             EnsureValidName(name);
@@ -21,20 +23,24 @@ namespace MiniWebServer.Abstractions.Http
             Name = name;
             Value = value ?? throw new ArgumentNullException(nameof(value));
 
+            Domain = domain;
             Expires = expires;
             Secure = secure;
             HttpOnly = httpOnly;
             MaxAge = maxAge;
             Path = path;
+            SameSite = sameSite;
         }
 
         public string Name { get; }
         public string Value { get; }
-        public DateTime? Expires { get; }
+        public string? Domain { get; }
+        public DateTimeOffset? Expires { get; }
         public bool? Secure { get; }
         public bool? HttpOnly { get; }
         public long? MaxAge { get; }
         public string? Path { get; }
+        public SameSitePolicies? SameSite { get; }
 
         public enum SameSitePolicies
         {
@@ -55,6 +61,50 @@ namespace MiniWebServer.Abstractions.Http
 
                 throw new InvalidOperationException();
             }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(Name);
+            sb.Append('=');
+            sb.Append(Value);
+
+            if (Domain != null)
+            {
+                sb.Append("; Domain=");
+                sb.Append(Domain);
+            }
+            if (Expires.HasValue)
+            {
+                sb.Append("; Expires=");
+                sb.Append(Expires.Value.UtcDateTime.ToString("ddd, dd MMM yyyy HH:mm:ss G\\MT"));
+            }
+            if (HttpOnly.HasValue)
+            {
+                sb.Append("; HttpOnly");
+            }
+            if (MaxAge.HasValue)
+            {
+                sb.Append("; Max-Age=");
+                sb.Append(MaxAge.Value);
+            }
+            if (Path != null)
+            {
+                sb.Append("; Path=");
+                sb.Append(Path);
+            }
+            if (SameSite != null)
+            {
+                sb.Append("; SameSite=");
+                sb.Append(SameSite.Value.ToString());
+            }
+            if (Secure.HasValue)
+            {
+                sb.Append("; Secure");
+            }
+
+            return sb.ToString();
         }
     }
 }
