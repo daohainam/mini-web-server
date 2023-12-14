4b4c2cbc470cd4934d8f55d565a0a9e13b0675a9
daohainam <daohainam@gmail.com>
2023-12-14 13:28:01 +0100

    Add AuthorizationHeaderParser

---
+++ MiniWebServer.Abstractions/Http/Header/Parsers/AuthorizationHeaderParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniWebServer.Abstractions.Http.Header.Parsers
+{
+    internal class AuthorizationHeaderParser : IHeaderParser
+    {
+        public object? Parse(string? value)
+        {
+            if (value == null) 
+                return null;
+
+            int idx = value.IndexOf(' ');
+            if (idx < 0 )
+            {
+                return new AuthorizationHeader()
+                {
+                    Scheme = value
+                };
+            }
+            else
+            {
+                return new AuthorizationHeader()
+                {
+                    Scheme = value[..idx],
+                    Parameters = value[(idx + 1)..]
+                };
+            }
+        }
+    }
+}
