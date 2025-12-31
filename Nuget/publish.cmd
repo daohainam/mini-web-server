f13f5bb2a00f215f66791694467fd98624d21482
Dao Hai Nam <daohainam@gmail.com>
2025-12-31 10:37:09 +0100

    Update version number 0.5.0 and nuget publish script

--- Nuget/publish.cmd
+++ Nuget/publish.cmd
@@ -1,21 +1,22 @@
 SET PublishProfile=Debug
+SET MiniWebServerVersion=0.5.0
 SET NugetSource=https://api.nuget.org/v3/index.json
 
-nuget push ..\Middleware\Authentication\bin\%PublishProfile%\MiniWebServer.Authentication.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\Middleware\Authorization\bin\%PublishProfile%\MiniWebServer.Authorization.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\Middleware\HttpsRedirection\bin\%PublishProfile%\MiniWebServer.HttpsRedirection.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\Middleware\Mvc\bin\%PublishProfile%\MiniWebServer.Mvc.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\Middleware\OutputCaching\bin\%PublishProfile%\MiniWebServer.OutputCaching.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\Middleware\Session\bin\%PublishProfile%\MiniWebServer.Session.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\Middleware\StaticFiles\bin\%PublishProfile%\MiniWebServer.StaticFiles.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\MiniWebServer.Abstractions\bin\%PublishProfile%\MiniWebServer.Abstractions.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\MiniWebServer.Configuration\bin\%PublishProfile%\MiniWebServer.Configuration.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\MiniWebServer.Helpers\bin\%PublishProfile%\MiniWebServer.Helpers.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\MiniWebServer.HttpParser\bin\%PublishProfile%\MiniWebServer.HttpParser.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\MiniWebServer.MimeMapping\bin\%PublishProfile%\MiniWebServer.MimeMapping.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\MiniWebServer.MiniApp\bin\%PublishProfile%\MiniWebServer.MiniApp.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\MiniWebServer.Mvc.Abstraction\bin\%PublishProfile%\MiniWebServer.Mvc.Abstraction.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\MiniWebServer.Mvc.MiniRazorEngine\bin\%PublishProfile%\MiniWebServer.Mvc.MiniRazorEngine.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\MiniWebServer.Mvc.RazorLightTemplateParser\bin\%PublishProfile%\MiniWebServer.Mvc.RazorLightTemplateParser.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\MiniWebServer.Server\bin\%PublishProfile%\MiniWebServer.Server.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
-nuget push ..\MiniWebServer.Server.Abstractions\bin\%PublishProfile%\MiniWebServer.Server.Abstractions.0.3.3.nupkg -Source %NugetSource% -SkipDuplicate
+dotnet nuget push ..\Middleware\Authentication\bin\%PublishProfile%\MiniWebServer.Authentication.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\Middleware\Authorization\bin\%PublishProfile%\MiniWebServer.Authorization.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\Middleware\HttpsRedirection\bin\%PublishProfile%\MiniWebServer.HttpsRedirection.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\Middleware\Mvc\bin\%PublishProfile%\MiniWebServer.Mvc.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\Middleware\OutputCaching\bin\%PublishProfile%\MiniWebServer.OutputCaching.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\Middleware\Session\bin\%PublishProfile%\MiniWebServer.Session.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\Middleware\StaticFiles\bin\%PublishProfile%\MiniWebServer.StaticFiles.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\MiniWebServer.Abstractions\bin\%PublishProfile%\MiniWebServer.Abstractions.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\MiniWebServer.Configuration\bin\%PublishProfile%\MiniWebServer.Configuration.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\MiniWebServer.Helpers\bin\%PublishProfile%\MiniWebServer.Helpers.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\MiniWebServer.HttpParser\bin\%PublishProfile%\MiniWebServer.HttpParser.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\MiniWebServer.MimeMapping\bin\%PublishProfile%\MiniWebServer.MimeMapping.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\MiniWebServer.MiniApp\bin\%PublishProfile%\MiniWebServer.MiniApp.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\MiniWebServer.Mvc.Abstraction\bin\%PublishProfile%\MiniWebServer.Mvc.Abstraction.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\MiniWebServer.Mvc.MiniRazorEngine\bin\%PublishProfile%\MiniWebServer.Mvc.MiniRazorEngine.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\MiniWebServer.Mvc.RazorLightTemplateParser\bin\%PublishProfile%\MiniWebServer.Mvc.RazorLightTemplateParser.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\MiniWebServer.Server\bin\%PublishProfile%\MiniWebServer.Server.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
+dotnet nuget push ..\MiniWebServer.Server.Abstractions\bin\%PublishProfile%\MiniWebServer.Server.Abstractions.%MiniWebServerVersion%.nupkg --source %NugetSource% --skip-duplicate
